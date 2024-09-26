d2da25141df0be98342b35d4a7cedccbc3152caf
x4mok <jeremy@thecollinshouse.com>
2024-09-26 11:41:58 +0100

    v0.1.0-STABLE : A Stable version of the mod which makes items obtainable, check the wiki to see obtainment

--- src/main/java/com/x4mok/depths_of_ruin/event/DragonDeath.java
+++ src/main/java/com/x4mok/depths_of_ruin/event/DragonDeath.java
@@ -17,54 +17,54 @@ import java.util.concurrent.ThreadLocalRandom;
 
 @Mod.EventBusSubscriber(modid = "depths_of_ruin")
 public class DragonDeath {
-    public static void sendPlayerMessage(PlayerEntity player, String message) {
-        // Create a text component from the message string
-        StringTextComponent textComponent = new StringTextComponent(TextFormatting.GREEN + message);
+	public static void sendPlayerMessage(PlayerEntity player, String message) {
+		// Create a text component from the message string
+		StringTextComponent textComponent = new StringTextComponent(TextFormatting.GREEN + message);
 
-        // Send the message to the player
-        player.sendMessage(textComponent, player.getUUID());
-    }
+		// Send the message to the player
+		player.sendMessage(textComponent, player.getUUID());
+	}
 
-    public static void dropItemInWorld(World world, PlayerEntity player, ItemStack itemStack) {
-        if (!world.isClientSide) { // Ensure you're on the server side
-            ItemEntity itemEntity = new ItemEntity(world, player.position().x, player.position().y, player.position().z, itemStack);
-            world.addFreshEntity(itemEntity);
-        }
-    }
+	public static void dropItemInWorld(World world, PlayerEntity player, ItemStack itemStack) {
+		if (!world.isClientSide) { // Ensure you're on the server side
+			ItemEntity itemEntity = new ItemEntity(world, player.position().x, player.position().y, player.position().z, itemStack);
+			world.addFreshEntity(itemEntity);
+		}
+	}
 
-    @SubscribeEvent
-    public static void onEnderDragonDeath(LivingDeathEvent event) {
-        Entity entity = event.getEntity();
-        if (entity instanceof EnderDragonEntity) {
-            EnderDragonEntity enderDragon = (EnderDragonEntity) entity;
-            PlayerEntity player = (PlayerEntity) enderDragon.getCombatTracker().getKiller();
-            if (player != null) {
-                // Give custom items to the player who dealt the final blow
-                ItemStack dragonscale = new ItemStack(ModItems.DRAGON_SCALE.get());
-                ItemStack dragonblood = new ItemStack(ModItems.DRAGON_BLOOD.get());
+	@SubscribeEvent
+	public static void onEnderDragonDeath(LivingDeathEvent event) {
+		Entity entity = event.getEntity();
+		if (entity instanceof EnderDragonEntity) {
+			EnderDragonEntity enderDragon = (EnderDragonEntity) entity;
+			PlayerEntity player = (PlayerEntity) enderDragon.getCombatTracker().getKiller();
+			if (player != null) {
+				// Give custom items to the player who dealt the final blow
+				ItemStack dragonscale = new ItemStack(ModItems.DRAGON_SCALE.get());
+				ItemStack dragonblood = new ItemStack(ModItems.DRAGON_BLOOD.get());
 
-                System.out.println("got itemstacks");
+				System.out.println("got itemstacks");
 
-                int randomInt = ThreadLocalRandom.current().nextInt(3, 9 + 1);
+				int randomInt = ThreadLocalRandom.current().nextInt(3, 9 + 1);
 
-                for (int i = 1; i <= randomInt; i++) {
-                    System.out.println("Iteration " + i);
-                    DragonDeath.dropItemInWorld(player.getEntity().level, player, new ItemStack(ModItems.DRAGON_SCALE.get()));
-                    System.out.println("DROPPED");
-                }
+				for (int i = 1; i <= randomInt; i++) {
+					System.out.println("Iteration " + i);
+					DragonDeath.dropItemInWorld(player.getEntity().level, player, new ItemStack(ModItems.DRAGON_SCALE.get()));
+					System.out.println("DROPPED");
+				}
 
-                randomInt = ThreadLocalRandom.current().nextInt(3, 9 + 1);
+				randomInt = ThreadLocalRandom.current().nextInt(3, 9 + 1);
 
-                for (int i = 1; i <= randomInt; i++) {
-                    System.out.println("Iteration " + i);
-                    DragonDeath.dropItemInWorld(player.getEntity().level, player, new ItemStack(ModItems.DRAGON_BLOOD.get()));
-                    System.out.println("DROPPED");
-                }
+				for (int i = 1; i <= randomInt; i++) {
+					System.out.println("Iteration " + i);
+					DragonDeath.dropItemInWorld(player.getEntity().level, player, new ItemStack(ModItems.DRAGON_BLOOD.get()));
+					System.out.println("DROPPED");
+				}
 
-                sendPlayerMessage(player, "A Dragon's Blood Dropped Near You!");
-                sendPlayerMessage(player, "A Dragon's Scale Dropped Near You!");
+				sendPlayerMessage(player, "A Dragon's Blood Dropped Near You!");
+				sendPlayerMessage(player, "A Dragon's Scale Dropped Near You!");
 
-            }
-        }
-    }
+			}
+		}
+	}
 }
--- src/main/java/com/x4mok/depths_of_ruin/item/ModItems.java
+++ src/main/java/com/x4mok/depths_of_ruin/item/ModItems.java
@@ -10,38 +10,38 @@ import net.minecraftforge.registries.ForgeRegistries;
 
 public class ModItems {
 
-    public static final DeferredRegister<Item> ITEMS =
-            DeferredRegister.create(ForgeRegistries.ITEMS, Depths_of_ruin.MODID);
+	public static final DeferredRegister<Item> ITEMS =
+			DeferredRegister.create(ForgeRegistries.ITEMS, Depths_of_ruin.MODID);
 
-    public static final RegistryObject<Item> DRAGON_BLOOD = ITEMS.register("dragon_blood",
-            () -> new Item(new Item.Properties().tab(ItemGroup.TAB_MATERIALS).stacksTo(64).rarity(Rarity.COMMON)));
-    public static final RegistryObject<Item> DRAGON_SCALE = ITEMS.register("dragon_scale",
-            () -> new Item(new Item.Properties().tab(ItemGroup.TAB_MATERIALS).stacksTo(64).rarity(Rarity.COMMON)));
-    public static final RegistryObject<Item> TRUE_DRAGON_SCALE = ITEMS.register("true_dragon_scale",
-            () -> new Item(new Item.Properties().tab(ItemGroup.TAB_MATERIALS).stacksTo(64).rarity(Rarity.UNCOMMON)));
-    public static final RegistryObject<Item> DRAGON_SWORD = ITEMS.register("dragon_sword",
-            () -> new SwordItem(ModItemTier.DRAGON, 3, -2.4F, (new Item.Properties().tab(ModItemGroup.DEPTHS_OF_RUIN_TAB).rarity(Rarity.COMMON))));
-    public static final RegistryObject<Item> DRAGON_AXE = ITEMS.register("dragon_axe",
-            () -> new AxeItem(ModItemTier.DRAGON, 6.5f, -3.0F, (new Item.Properties().tab(ModItemGroup.DEPTHS_OF_RUIN_TAB).rarity(Rarity.COMMON))));
-    public static final RegistryObject<Item> DRAGON_PICKAXE = ITEMS.register("dragon_pickaxe",
-            () -> new PickaxeItem(ModItemTier.DRAGON, 1, -2.8F, (new Item.Properties().tab(ModItemGroup.DEPTHS_OF_RUIN_TAB).rarity(Rarity.COMMON))));
-    public static final RegistryObject<Item> DRAGON_SHOVEL = ITEMS.register("dragon_shovel",
-            () -> new ShovelItem(ModItemTier.DRAGON, 1.5f, -3.0F, (new Item.Properties().tab(ModItemGroup.DEPTHS_OF_RUIN_TAB).rarity(Rarity.COMMON))));
-    public static final RegistryObject<Item> DRAGON_HOE = ITEMS.register("dragon_hoe",
-            () -> new HoeItem(ModItemTier.DRAGON, 0, -2.0F, (new Item.Properties().tab(ModItemGroup.DEPTHS_OF_RUIN_TAB).rarity(Rarity.COMMON))));
+	public static final RegistryObject<Item> DRAGON_BLOOD = ITEMS.register("dragon_blood",
+			() -> new Item(new Item.Properties().tab(ItemGroup.TAB_MATERIALS).stacksTo(64).rarity(Rarity.COMMON)));
+	public static final RegistryObject<Item> DRAGON_SCALE = ITEMS.register("dragon_scale",
+			() -> new Item(new Item.Properties().tab(ItemGroup.TAB_MATERIALS).stacksTo(64).rarity(Rarity.COMMON)));
+	public static final RegistryObject<Item> TRUE_DRAGON_SCALE = ITEMS.register("true_dragon_scale",
+			() -> new Item(new Item.Properties().tab(ItemGroup.TAB_MATERIALS).stacksTo(64).rarity(Rarity.UNCOMMON)));
+	public static final RegistryObject<Item> DRAGON_SWORD = ITEMS.register("dragon_sword",
+			() -> new SwordItem(ModItemTier.DRAGON, 3, -2.4F, (new Item.Properties().tab(ModItemGroup.DEPTHS_OF_RUIN_TAB).rarity(Rarity.COMMON))));
+	public static final RegistryObject<Item> DRAGON_AXE = ITEMS.register("dragon_axe",
+			() -> new AxeItem(ModItemTier.DRAGON, 6.5f, -3.0F, (new Item.Properties().tab(ModItemGroup.DEPTHS_OF_RUIN_TAB).rarity(Rarity.COMMON))));
+	public static final RegistryObject<Item> DRAGON_PICKAXE = ITEMS.register("dragon_pickaxe",
+			() -> new PickaxeItem(ModItemTier.DRAGON, 1, -2.8F, (new Item.Properties().tab(ModItemGroup.DEPTHS_OF_RUIN_TAB).rarity(Rarity.COMMON))));
+	public static final RegistryObject<Item> DRAGON_SHOVEL = ITEMS.register("dragon_shovel",
+			() -> new ShovelItem(ModItemTier.DRAGON, 1.5f, -3.0F, (new Item.Properties().tab(ModItemGroup.DEPTHS_OF_RUIN_TAB).rarity(Rarity.COMMON))));
+	public static final RegistryObject<Item> DRAGON_HOE = ITEMS.register("dragon_hoe",
+			() -> new HoeItem(ModItemTier.DRAGON, 0, -2.0F, (new Item.Properties().tab(ModItemGroup.DEPTHS_OF_RUIN_TAB).rarity(Rarity.COMMON))));
 
-    public static final RegistryObject<Item> DRAGON_HELMET = ITEMS.register("dragon_helmet",
-            () -> new ArmorItem(ModArmourMaterial.DRAGON, EquipmentSlotType.HEAD, (new Item.Properties().tab(ModItemGroup.DEPTHS_OF_RUIN_TAB).rarity(Rarity.COMMON))));
-    public static final RegistryObject<Item> DRAGON_CHESTPLATE = ITEMS.register("dragon_chestplate",
-            () -> new ArmorItem(ModArmourMaterial.DRAGON, EquipmentSlotType.CHEST, (new Item.Properties().tab(ModItemGroup.DEPTHS_OF_RUIN_TAB).rarity(Rarity.COMMON))));
-    public static final RegistryObject<Item> DRAGON_LEGGINGS = ITEMS.register("dragon_leggings",
-            () -> new ArmorItem(ModArmourMaterial.DRAGON, EquipmentSlotType.LEGS, (new Item.Properties().tab(ModItemGroup.DEPTHS_OF_RUIN_TAB).rarity(Rarity.COMMON))));
-    public static final RegistryObject<Item> DRAGON_BOOTS = ITEMS.register("dragon_boots",
-            () -> new ArmorItem(ModArmourMaterial.DRAGON, EquipmentSlotType.FEET, (new Item.Properties().tab(ModItemGroup.DEPTHS_OF_RUIN_TAB).rarity(Rarity.COMMON))));
+	public static final RegistryObject<Item> DRAGON_HELMET = ITEMS.register("dragon_helmet",
+			() -> new ArmorItem(ModArmourMaterial.DRAGON, EquipmentSlotType.HEAD, (new Item.Properties().tab(ModItemGroup.DEPTHS_OF_RUIN_TAB).rarity(Rarity.COMMON))));
+	public static final RegistryObject<Item> DRAGON_CHESTPLATE = ITEMS.register("dragon_chestplate",
+			() -> new ArmorItem(ModArmourMaterial.DRAGON, EquipmentSlotType.CHEST, (new Item.Properties().tab(ModItemGroup.DEPTHS_OF_RUIN_TAB).rarity(Rarity.COMMON))));
+	public static final RegistryObject<Item> DRAGON_LEGGINGS = ITEMS.register("dragon_leggings",
+			() -> new ArmorItem(ModArmourMaterial.DRAGON, EquipmentSlotType.LEGS, (new Item.Properties().tab(ModItemGroup.DEPTHS_OF_RUIN_TAB).rarity(Rarity.COMMON))));
+	public static final RegistryObject<Item> DRAGON_BOOTS = ITEMS.register("dragon_boots",
+			() -> new ArmorItem(ModArmourMaterial.DRAGON, EquipmentSlotType.FEET, (new Item.Properties().tab(ModItemGroup.DEPTHS_OF_RUIN_TAB).rarity(Rarity.COMMON))));
 
 
 
-    public static void register(IEventBus eventBus) {
-        ITEMS.register(eventBus);
-    }
+	public static void register(IEventBus eventBus) {
+		ITEMS.register(eventBus);
+	}
 }
